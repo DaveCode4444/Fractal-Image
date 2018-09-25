2ba036b21e0905f1f72d862c6ed859bc1723429a
Devang <sawantdevang9@gmail.com>
2018-09-25 07:25:40 -0400

    First Commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * This project is on the topic of how to create fractal images
+ * main.cpp
+ *
+ *  Created on: Sep 25, 2018
+ *      Author: Devang Sawant
+ */
+#include <iostream>
+
+int main()
+{
+	std::cout << "Fractal Image" << std::endl;
+	return 0;
+}
